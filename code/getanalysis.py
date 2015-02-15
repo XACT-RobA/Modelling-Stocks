346ca8808a99ea4bd8093aeb50977ed2d9970f8f
Robert Anderson <robertanderson1993@gmail.com>
2015-02-15 20:05:08 +0000

    Basic and reversal analysis
    Created a python file to do basic analysis of the data, and updated the
    code that looks at reversals

---
+++ code/getanalysis.py
@@ -0,0 +1,74 @@
+import csv
+
+def import_j_candles(filepath):
+    # Open & read the csv file
+    with open(filepath, 'rb') as jcandle_file:
+        data = []
+        # Read the csv file
+        jcandle_data = csv.reader(jcandle_file, delimiter=',')
+        # Iterate through the rows
+        for row in jcandle_data:
+            # Using standard open-high-low-close stock data
+            j_open = float(row[1])
+            j_high = float(row[2])
+            j_low = float(row[3])
+            j_close = float(row[4])
+            # Store the data
+            data.append([j_open, j_high, j_low, j_close])
+    return data
+
+def body_and_shadow(data):
+    # Number of candles
+    data_length = len(data)
+    # Gain or loss
+    increase_data = [0]*data_length
+    # Body sizes
+    body_sizes = [0]*data_length
+    # Shadow sizes
+    shadow_sizes = [0]*data_length
+    # Iterate through candles
+    for i in range(data_length):
+        # Get current open & close
+        c_open = data[i][0]
+        c_high = data[i][1]
+        c_low = data[i][2]
+        c_close = data[i][3]
+        # Calculate body size
+        this_body_size = c_close-c_open
+        # Store body sizes
+        body_sizes[i] += abs(this_body_size)
+        # Work out whether this candle is a gain or a loss
+        this_increase = (this_body_size >= 0)
+        # Flag candle as gain
+        if this_increase:
+            increase_data[i] += 1
+        # Calculate shadow size
+        this_shadow_size = c_high-c_close
+        shadow_sizes[i] += this_shadow_size
+    return ([increase_data, body_sizes, shadow_sizes, data_length])
+
+def runs_and_reversal(data, data_length, increase_data):
+    # Get run lengths & reversals
+    run_length_data = [None]*data_length
+    run_length = 0
+    reversals = [0]*data_length
+    # Iterate through candles
+    for i in range(data_length):
+        # Set first candle as 0
+        if i > 0:
+            # If reversal
+            if increase_data[i] <> increase_data[i-1]:
+                # Reset run length
+                run_length = 0
+                # Flag reversal
+                reversals[i] += 1
+        # Set current run length
+        run_length_data[i] = run_length
+        # Iterate
+        run_length += 1
+    return ([run_length_data, reversals])
+
+def analyse(data):
+    [increase_data, body_sizes, shadow_sizes, data_length] = body_and_shadow(data)
+    [run_length_data, reversals] = runs_and_reversal(data, data_length, increase_data)
+    return ([increase_data, body_sizes, shadow_sizes, data_length, run_length_data, reversals])
--- code/reversalanalysis.py
+++ code/reversalanalysis.py
@@ -1,54 +1,31 @@
-import csv
+import getanalysis
+import tradesim
 
-# Open & read the csv file
-with open('../data/jcandles.csv', 'rb') as jcandle_file:
-    data = []
-    # Read the csv file
-    jcandle_data = csv.reader(jcandle_file, delimiter=',')
-    # Iterate through the rows
-    for row in jcandle_data:
-        # Using standard open-high-low-close stock data
-        j_open = float(row[1])
-        j_high = float(row[2])
-        j_low = float(row[3])
-        j_close = float(row[4])
-        # Store the data
-        data.append([j_open, j_high, j_low, j_close])
-        
-# Number of candles
-data_length = len(data)
+data = getanalysis.import_j_candles('../data/jcandles.csv')
 
-# Gain or loss
-increase_data = [0]*data_length
-# Iterate through candles
+[increase_data, body_sizes, shadow_sizes, data_length, run_length_data, reversals] = getanalysis.analyse(data)
+
+
+############
+
+# Buy or sell on every reversal
+
+trade_array = [0]*data_length
+last_trade = 0
+
 for i in range(data_length):
-    # Get current open & close
-    c_open = data[i][0]
-    c_close = data[i][3]
-    # Calculate body size
-    body_size = c_close-c_open
-    # Work out whether this candle is a gain or a loss
-    this_increase = (body_size >= 0)
-    # Flag candle as gain
-    if this_increase:
-        increase_data[i] += 1
-
-# Get run lengths & reversals
-run_length_data = [None]*data_length
-run_length = 0
-reversals = [0]*data_length
-# Iterate through candles
-for i in range(data_length):
-    # Set first candle as 0
-    if i > 0:
-        # If reversal
-        if increase_data[i] <> increase_data[i-1]:
-            # Reset run length
-            run_length = 0
-            # Flag reversal
-            reversals[i] += 1
-    # Set current run length
-    run_length_data[i] = run_length
-    # Iterate
-    run_length += 1
-
+    if i == 0:
+        if increase_data[i] == 1:
+            # Buy
+            trade_array[i] = 1
+            last_trade = 1
+    else:
+        if run_length_data[i] == 0:
+            trade_array[i] = last_trade + 1
+            last_trade = (last_trade + 1) % 2
+            
+[profit, profit_array] = tradesim.sim_trade(data, trade_array)
+percent_profit = (profit - 1) * 100
+print('Buying and selling every reversal')
+print('Profit: ' + str(percent_profit) + '%')
+print('Max profit: ' + str(max(profit_array)) + '%')
