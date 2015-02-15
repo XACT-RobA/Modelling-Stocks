1297677de9b9464c3bcedc4cb7bb6b9822cd3df2
Robert Anderson <robertanderson1993@gmail.com>
2015-02-15 20:02:37 +0000

    Created trade simulator for testing

---
+++ code/tradesim.py
@@ -0,0 +1,28 @@
+def sim_trade(data, trade_array):
+    data_length = len(data)
+    if data_length == len(trade_array):
+        value = 1.0
+        last_trade = 0
+        buy_price = 0
+        sell_price = 0
+        profit_array = [0] * data_length
+        for i in range(data_length):
+            # Buy and sell at the closing price
+            if trade_array[i] == 1:
+                # Buy
+                # Check not already bought
+                if last_trade == 0:
+                    buy_price = data[i][3]
+                    last_trade = 1
+            elif trade_array[i] == 2:
+                # Sell
+                # Check not already sold
+                if last_trade == 1:
+                    sell_price = data[i][3]
+                    value = value * (sell_price / buy_price)
+                    profit_array[i] += value
+                    last_trade = 0
+        return [value, profit_array]
+    else:
+        print('Error: Data and trade array are different lengths')
+        return [0, 0]
