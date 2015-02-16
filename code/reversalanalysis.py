3d7e51886eb46e8f83fc9c94bdba83608d4f960e
Robert Anderson <robertanderson1993@gmail.com>
2015-02-16 01:10:18 +0000

    Added new buy/sell simulation
    Added code to buy at Doji reversal, and sell at the next reversal

--- code/reversalanalysis.py
+++ code/reversalanalysis.py
@@ -1,18 +1,28 @@
 import getanalysis
 import tradesim
 
+# Import japanese candle data
 data = getanalysis.import_j_candles('../data/jcandles.csv')
 
+# Get all sorts of data
 [increase_data, body_sizes, shadow_sizes, data_length, run_length_data, reversals] = getanalysis.analyse(data)
 
+# Get body size data
+[av_body_size, body_size_quartiles] = getanalysis.body_size_analysis(body_sizes)
 
-############
+# Get Dojis
+doji_array = [0]*data_length
+for i in range(data_length):
+    if body_sizes[i] < body_size_quartiles[1]:
+        doji_array[i] += 1
+        
+###########
+# Testing #
+###########
 
 # Buy or sell on every reversal
-
 trade_array = [0]*data_length
 last_trade = 0
-
 for i in range(data_length):
     if i == 0:
         if increase_data[i] == 1:
@@ -28,4 +38,34 @@ for i in range(data_length):
 percent_profit = (profit - 1) * 100
 print('Buying and selling every reversal')
 print('Profit: ' + str(percent_profit) + '%')
-print('Max profit: ' + str(max(profit_array)) + '%')
+print('Max profit: ' + str(max(profit_array)) + '%\n')
+
+
+# Buy at Doji reversal, sell at reversal
+trade_array = [0]*data_length
+last_trade = 0
+for i in range(data_length):
+    # Reversal
+    if run_length_data[i] == 0:
+        # Gain
+        if increase_data[i] == 1:
+            # Sold last
+            if last_trade == 0:
+                # Doji
+                if doji_array[i] == 1:
+                    #Buy
+                    trade_array[i] = 1
+                    last_trade = 1
+        # Loss
+        else:
+            # Bought last
+            if last_trade == 1:
+                #Sell
+                trade_array[i] = 2
+                last_trade = 0
+                
+[profit, profit_array] = tradesim.sim_trade(data, trade_array)
+percent_profit = (profit - 1) * 100
+print('Buying at Doji reversal, selling at reversal')
+print('Profit: ' + str(percent_profit) + '%')
+print('Max profit: ' + str(max(profit_array)) + '%\n')
