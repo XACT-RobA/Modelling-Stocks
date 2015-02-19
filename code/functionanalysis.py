f76f2372ceae3e7aecafc11695ebe000113af171
XACT-RobA <robertanderson1993@gmail.com>
2015-02-19 01:32:44 +0000

    Started looking at gains after reversal as a function

--- code/functionanalysis.py
+++ code/functionanalysis.py
@@ -10,33 +10,48 @@ data = getanalysis.import_j_candles(filepath)
 
 run_function_data = [None]*data_length
 
+run_diff_array = [0]*data_length
+cumulative_run_length = 0
+for i in range(data_length):
+    if i == 0:
+        cumulative_run_diff = 0
+    cumulative_run_diff += body_sizes[i]
+    run_diff_array[i] += cumulative_run_diff
+    
 for i in range(data_length):
     if run_length_data[i] == 0:
         prev_run_length = 0
         prev_run_diff = 0
         if i > 0:
-            try:
-                prev_run_length = run_length_data[i-1] + 1
-                prev_run_diff = sum(body_sizes[(i-(prev_run_length+1)):(i-1)])
-            except:
-                pass
-        if i < data_length - 1:
-            next_run_length = 0
-            next_run_diff = 0
-            try:
-                j = 1
-                while j < max(run_length_data) + 1:
-                    if run_length_data[i+j] == 0:
-                        next_run_length = run_length_data[i+j-1] + 1
-                        next_run_diff = sum(body_sizes[i:(i+next_run_length - 1)])
-                        j = max(run_length_data) + 1
-                    j += 1
-            except:
-                pass
-    
-        run_function_data[i] = [i, prev_run_length, prev_run_diff, next_run_length, next_run_diff]
+            prev_run_length = run_length_data[i-1] + 1
+            prev_run_diff = run_diff_array[i-1]
+        next_run_length = 0
+        next_run_diff = 0
+        for j in range(max(run_length_data) - 1):
+            k = j + 1
+            if (i+k) < data_length:
+                if run_length_data[i+k] == 0:
+                    next_run_length = run_length_data[i+j]
+                    next_run_diff = run_diff_array[i+j]
+                    break
+'''
+        run_function_data[i] = [i, prev_run_length, prev_run_diff, next_run_length, next_run_diff, increase_data[i]]
+    else:
+        run_function_data[i] = [0, 0, 0, 0, 0, increase_data[i]]
 
 with open('../data/run_function_data.csv', 'wb') as run_data_file:
+    run_data_writer = csv.writer(run_data_file, delimiter=',')
+    for run_data in run_function_data:
+        try:
+            run_data_writer.writerow(run_data)
+        except:
+            pass        
+'''
+
+        if increase_data[i] == 1:
+            run_function_data[i] = [i, prev_run_length, prev_run_diff, next_run_length, next_run_diff]
+
+with open('../data/gain_function_data.csv', 'wb') as run_data_file:
     run_data_writer = csv.writer(run_data_file, delimiter=',')
     for run_data in run_function_data:
         try:
